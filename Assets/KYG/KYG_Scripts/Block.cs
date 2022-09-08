a9d7a6c0eb9141232190d632f7e37fc1c6f23093
Ellian <dbsrn0125@naver.com>
2022-09-08 13:21:35 +0900

    abc

---
+++ Assets/KYG/KYG_Scripts/Block.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    //땅 주인
+    public GameObject LandOwner;
+    
+    //통행료
+    public int charge;
+
+    //땅표시 공장
+    public GameObject landFlagFactory;
+
+    //object1공장
+    public GameObject object1Factory;
+
+    //object2공장
+    public GameObject object2Factory;
+
+    //object3공장
+    public GameObject object3Factory;
+
+    //landMark 공장
+    public GameObject landMarkFactory;
+
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
