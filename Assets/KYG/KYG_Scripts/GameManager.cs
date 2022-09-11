19c61a0d425717d21bbb137bf8886adae4dc6575
Ellian <62099455+ygsh0315@users.noreply.github.com>
2022-09-12 01:53:12 +0900

    a

---
+++ Assets/KYG/KYG_Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private void Awake()
+    {
+        if(!instance)
+        {
+            instance = this;
+        }
+    }
+    public int round = 0;
+    List<GameObject> MapList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
