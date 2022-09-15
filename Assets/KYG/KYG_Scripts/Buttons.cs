0b89aeddc3bda2810a92769b9e6408b06dda5eeb
JasonShin10 <101558745+JasonShin10@users.noreply.github.com>
2022-09-15 23:40:37 +0900

    .

--- Assets/KYG/KYG_Scripts/Buttons.cs
+++ Assets/KYG/KYG_Scripts/Buttons.cs
@@ -6,11 +6,15 @@ using TMPro;
 
 public class Buttons : MonoBehaviour
 {
+    
     public TextMeshProUGUI dice1Number;
     public TextMeshProUGUI dice2Number;
+    public GameObject even;
+    public GameObject odd;
+    DicePower dicePower;
     void Start()
     {
-        
+        dicePower = GetComponent<DicePower>();   
     }
 
     // Update is called once per frame
@@ -18,14 +22,78 @@ public class Buttons : MonoBehaviour
     {
         
     }
-    public void RollDice()
+    //public void RollDice()
+    //{
+    //    int dice1 = Random.Range(6, 7);
+    //    int dice2 = Random.Range(6, 7);
+    //    dice1Number.text = dice1.ToString();
+    //    dice2Number.text = dice2.ToString();
+    //    GameManager.instance.currentTurnPlayer.GetComponent<Player>().PlayerMove(dice1, dice2);
+        
+    //}
+
+    public void PointerDown()
     {
-        int dice1 = Random.Range(6, 7);
-        int dice2 = Random.Range(6, 7);
+        dicePower.diceButton = false;
+        dicePower.power = 0;
+    }
+    
+    public void PointerUp()
+    {
+        //int dice1 = Random.Range(6, 7);
+        //int dice2 = Random.Range(6, 7);
+        int dice1 =0;
+        int dice2 =0;
+        
+        float value = GetComponent<DicePower>().power;
+        if (even.GetComponent<EvenOdd>().even == true)
+        {
+            if (value >= 0 && value <= 33)
+            {
+                dice1 = Random.Range(1, 3);
+                dice2 = Random.Range(1, 3);
+            }
+            if (value >= 34 && value <= 66)
+            {
+                {
+                    dice1 = Random.Range(3, 5);
+                    dice2 = Random.Range(3, 5);
+                }
+            }
+            if (value >= 67 && value <= 100)
+            {
+                {
+                    dice1 = Random.Range(4, 7);
+                    dice2 = Random.Range(4, 7);
+
+                }
+            }
+        }
+        if (value >=0 && value <=33)
+        {
+            dice1 = Random.Range(1, 3);
+            dice2 = Random.Range(1, 3);
+        }
+        if (value >=34 && value <= 66)
+        {
+            {
+                dice1 = Random.Range(3, 5);
+                dice2 = Random.Range(3, 5);
+            }
+        }
+        if (value >= 67 && value <= 100)
+        {
+            {
+                dice1 = Random.Range(4, 7);
+                dice2 = Random.Range(4, 7);
+
+            }
+        }
         dice1Number.text = dice1.ToString();
         dice2Number.text = dice2.ToString();
         GameManager.instance.currentTurnPlayer.GetComponent<Player>().PlayerMove(dice1, dice2);
-        GetComponent<DicePower>().diceButton = true;
+        dicePower.diceButton = true;
         
+
     }
 }
---
+++ Assets/KYG/KYG_Scripts/EvenOdd.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvenOdd : MonoBehaviour
+{
+    public bool even = false;
+    public bool odd = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onClick()
+    {
+        if(gameObject.name == "EvenButton")
+        {
+            even = true;
+        }
+        else
+        {
+
+        }
+
+    }
+}
