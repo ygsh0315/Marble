ec056bb7ac65af53e3edcdef9951727c07c87b8c
ssb5783 <110151488+ssb5783@users.noreply.github.com>
2022-09-18 21:11:26 +0900

    220918

---
+++ Assets/SSB/SSB_Script/RoomItem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomItem : MonoBehaviour
+{
+    // 내용 (방 이름 (0/0))
+    public Text roomInfo;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
