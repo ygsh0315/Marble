906e16ed4ccfd90aac8cf5b94234d1d2093ba234
Ellian <62099455+ygsh0315@users.noreply.github.com>
2022-09-19 03:33:51 +0900

    abc

--- Assets/KYG/KYG_Scripts/UI/TrapBlockUI.cs
+++ Assets/KYG/KYG_Scripts/UI/TrapBlockUI.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class TrapBlockUI : MonoBehaviour
 {
+    public Button payMoney;
+    public GameObject playerInfo;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,30 @@ public class TrapBlockUI : MonoBehaviour
     void Update()
     {
         
+    }
+
+    internal void process(GameObject player)
+    {
+        playerInfo = player;
+        if (player.GetComponent<Player>().money < 200000)
+        {
+            payMoney.interactable = false;
+        }
+    }
+
+    public void OnPayMoneyBtn()
+    {
+        playerInfo.GetComponent<Player>().money -= 200000;
+        playerInfo.GetComponent<Player>().trapCount = 0;
+    }
+
+    public void OnPayCardBtn()
+    {
+        print("payCard");
+    }
+
+    public void OnRollDiceBtn()
+    {
+
     }
 }
