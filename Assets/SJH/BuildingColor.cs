8f6185536b5137f66394af5a5858a1b2382bd530
JasonShin10 <101558745+JasonShin10@users.noreply.github.com>
2022-09-22 22:08:10 +0900

    .

---
+++ Assets/SJH/BuildingColor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingColor : MonoBehaviour
+{
+    public Material[] materials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       materials = Resources.LoadAll<Material>("Resources");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
