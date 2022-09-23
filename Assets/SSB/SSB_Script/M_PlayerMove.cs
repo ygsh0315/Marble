e23b416ec73530827ba93a489b2ab7c26a8257a4
ssb5783 <110151488+ssb5783@users.noreply.github.com>
2022-09-23 09:34:13 +0900

    hh

--- Assets/SSB/SSB_Script/M_PlayerMove.cs
+++ Assets/SSB/SSB_Script/M_PlayerMove.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//WASD 앞뒤좌우로 움직이게 한다 
+//WASD 앞뒤좌우로 움직이게 한다
+//필요속성 : 이동속도 
+
+//Stage밖으로 벗어나면 죽는다
+//필요속성 : Stage
+
+
 public class M_PlayerMove : MonoBehaviour
 {
     //이동속도
-    public float speed = 3; 
+    public float speed = 3;
+
+    //Stage
+    public Transform stage;
+
+    //dieDistance
+    public float dieDistance;
     void Start()
     {
         
@@ -15,6 +27,8 @@ public class M_PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        Die();
         //WASD
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
@@ -23,5 +37,19 @@ public class M_PlayerMove : MonoBehaviour
         dir.Normalize();
         //움직이게 한다
         transform.position += dir * speed * Time.deltaTime;
+    }
+
+    
+
+    public void Die()
+    {
+        dieDistance = Vector3.Distance(stage.position, transform.position);
+        //만약 스테이지를 벗어나게되면
+        if( dieDistance >5f)
+        {
+            //죽음처리한다
+            Destroy(gameObject, 2);
+        }
+
     }
 }
