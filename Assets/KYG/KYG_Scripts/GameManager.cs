86cbed9a59712d8a726b05b909d904bda0a4cde7
JasonShin10 <101558745+JasonShin10@users.noreply.github.com>
2022-09-29 13:55:18 +0900

    .

--- Assets/KYG/KYG_Scripts/GameManager.cs
+++ Assets/KYG/KYG_Scripts/GameManager.cs
@@ -60,8 +60,8 @@ public class GameManager : MonoBehaviourPun
         TrapCheck();
         
         Winner();
-        turnCalculate();
-        ChangeCurrentTurnPlayer(turnIndex);
+        //turnCalculate();
+        //ChangeCurrentTurnPlayer(turnIndex);
         ColorMonopolyCheck();
         RankCalCulate();
     }
